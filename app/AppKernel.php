6996557c717bd80539e5102d2b3e71a070ede0fb
Samuel Chiriluta <samuel4x4@users.noreply.github.com>
2016-06-16 10:23:10 +0300

    Update AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,6 +5,13 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+    public function __construct($environment, $debug)
+    {
+        date_default_timezone_set( 'Europe/Bucharest' );
+        
+        parent::__construct($environment, $debug);
+    }    
+    
     public function registerBundles()
     {
         $bundles = [
