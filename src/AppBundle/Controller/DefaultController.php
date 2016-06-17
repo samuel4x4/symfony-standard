5b6506c164cd8cdf06b89dc7a7048862f7c80eb2
Samuel Chiriluta <samuel4x4@users.noreply.github.com>
2016-06-17 09:31:48 +0300

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -45,6 +45,8 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $this->redirectToRoute($route);
+        
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
