cdfc19014875b8efa5fdcfbef9a270edb22f1f76
Samuel Chiriluta <samuel4x4@users.noreply.github.com>
2016-06-17 09:33:51 +0300

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -14,38 +13,11 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/task", name="task")
-     * @Template()
-     */
-    public function newAction(Request $request)
-    {
-        // just setup a fresh $task object (remove the dummy data)
-        $task = new Task();
-    
-        $form = $this->createFormBuilder($task)
-            ->add('task', TextType::class)
-            ->add('dueDate', DateType::class)
-            ->add('save', SubmitType::class, array('label' => 'Create Task'))
-            ->getForm();
-    
-        $form->handleRequest($request);
-    
-        if ($form->isSubmitted() && $form->isValid()) {
-            dump($task);
-            dump($request);
-            exit;
-            return $this->redirectToRoute('task_success');
-        }
-    
-        return ['form' => $form->createView()];
-    }  
-    
-    /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        $this->redirectToRoute($route);
+        $this->redirectToRoute('app_task_new');
         
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
