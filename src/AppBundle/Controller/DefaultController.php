56ec347def7b4b2147d703c64d6d62aa40fc9355
Samuel Chiriluta <samuel4x4@users.noreply.github.com>
2016-06-17 08:44:17 +0300

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -13,18 +14,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
-    }
-    
-    /**
      * @Route("/task", name="task")
+     * @Template()
      */
     public function newAction(Request $request)
     {
@@ -46,8 +37,18 @@ class DefaultController extends Controller
             return $this->redirectToRoute('task_success');
         }
     
-        return $this->render('default/new.html.twig', array(
-            'form' => $form->createView(),
-        ));
+        return ['form' => $form->createView()];
     }  
+    
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+    
 }
